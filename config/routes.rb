505e210fe1dcab698d8d2c03e65c1e66e66542e3
Juan Guzman <juan.guzman07@gmail.com>
2018-06-03 02:51:02 +0000

    added user_types database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :user_types
   get 'pages/patientforms'
   get 'pages/contact'
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180603022734) do
+ActiveRecord::Schema.define(version: 20180603024314) do
+
+  create_table "user_types", force: :cascade do |t|
+    t.string   "type_of_user"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
